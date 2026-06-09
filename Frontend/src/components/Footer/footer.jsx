90d8683c8c69c8f27edd6c8fdbc2007b4d8c3de1
Tấn Hiệp <hiepkt8018@ut.edu.vn>
2026-06-09 21:33:08 +0700

    frontend

--- Frontend/src/components/Footer/footer.jsx
+++ Frontend/src/components/Footer/footer.jsx
@@ -8,43 +8,60 @@ function Footer() {
         <div className="footer-column">
           <h3 className="footer_heading">CHĂM SÓC KHÁCH HÀNG</h3>
           <ul className="footer_list">
-            <li><a href="#" className="footer-item__link">Trung Tâm Trợ Giúp</a></li>
-            <li><a href="#" className="footer-item__link">Velora Store</a></li>
-            <li><a href="#" className="footer-item__link">Hướng dẫn mua hàng</a></li>
+            <li><a href="javascript:void(0)" className="footer-item__link">Trung Tâm Trợ Giúp</a></li>
+            <li><a href="javascript:void(0)" className="footer-item__link">Velora Store</a></li>
+            <li><a href="javascript:void(0)" className="footer-item__link">Hướng dẫn mua hàng</a></li>
           </ul>
         </div>
 
         <div className="footer-column">
           <h3 className="footer_heading">THÔNG TIN CỬA HÀNG</h3>
           <ul className="footer_list">
-            <li><a href="#" className="footer-item__link">Địa chỉ: Trường Đại Học Giao Thông Vận Tải TP.HCM</a></li>
-            <li><a href="#" className="footer-item__link">Số điện thoại:0352.456.789</a></li>
-            <li><a href="#" className="footer-item__link">Điều khoản</a></li>
+            <li>
+              <a href="javascript:void(0)" className="footer-item__link">
+                Địa chỉ: Trường Đại Học Giao Thông Vận Tải TP.HCM
+              </a>
+            </li>
+            <li>
+              <a href="javascript:void(0)" className="footer-item__link">
+                Số điện thoại:0352.456.789
+              </a>
+            </li>
+            <li><a href="javascript:void(0)" className="footer-item__link">Điều khoản</a></li>
           </ul>
         </div>
 
         <div className="footer-column">
           <h3 className="footer_heading">DANH MỤC</h3>
           <ul className="footer_list">
-            <li><a href="#" className="footer-item__link">Quần áo nam</a></li>
-            <li><a href="#" className="footer-item__link">Quần áo nữ</a></li>
-            <li><a href="#" className="footer-item__link">Áo khoác</a></li>
-            <li><a href="#" className="footer-item__link">Giày</a></li>
+            <li><a href="javascript:void(0)" className="footer-item__link">Quần áo nam</a></li>
+            <li><a href="javascript:void(0)" className="footer-item__link">Quần áo nữ</a></li>
+            <li><a href="javascript:void(0)" className="footer-item__link">Áo khoác</a></li>
+            <li><a href="javascript:void(0)" className="footer-item__link">Giày</a></li>
           </ul>
         </div>
 
         <div className="footer-column">
           <h3 className="footer_heading">THEO DÕI</h3>
           <ul className="footer_list">
-            <li><a href="#" className="footer-item__link">
-              <img src="/src/assets/images/fb.png" alt="Fb"/>
-              Facebook</a></li>
-            <li><a href="#" className="footer-item__link">
-              <img src="/src/assets/images/instagram.png" alt="ins"/>
-              Instagram</a></li>
-            <li><a href="#" className="footer-item__link">
-              <img src="/src/assets/images/linkedin.png" alt="linked"/>
-              LinkedIn</a></li>
+            <li>
+              <a href="javascript:void(0)" className="footer-item__link">
+                <img src="/src/assets/images/fb.png" alt="Fb"/>
+                Facebook
+              </a>
+            </li>
+            <li>
+              <a href="javascript:void(0)" className="footer-item__link">
+                <img src="/src/assets/images/instagram.png" alt="ins"/>
+                Instagram
+              </a>
+            </li>
+            <li>
+              <a href="javascript:void(0)" className="footer-item__link">
+                <img src="/src/assets/images/linkedin.png" alt="linked"/>
+                LinkedIn
+              </a>
+            </li>
           </ul>
         </div>
 
